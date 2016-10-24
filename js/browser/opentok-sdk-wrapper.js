32e06f1c6715d93e69952d5e69a9d12656f3789f
Aaron Rice <adrice727@gmail.com>
2016-10-24 12:21:44 -0700

    Remove unused events.

--- js/browser/opentok-sdk-wrapper.js
+++ js/browser/opentok-sdk-wrapper.js
@@ -2,13 +2,7 @@
 'use strict';
 
 var events = {
-  session: ['archiveStarted', 'archiveStopped', 'connectionCreated', 'connectionDestroyed', 'sessionConnected', 'sessionDisconnected', 'sessionReconnected', 'sessionReconnecting', 'signal', 'streamCreated', 'streamDestroyed', 'streamPropertyChanged'],
-  core: ['connected', 'startScreenShare', 'endScreenShare', 'error'],
-  communication: ['startCall', 'endCall', 'callPropertyChanged', 'subscribeToCamera', 'subscribeToScreen', 'unsubscribeFromCamera', 'unsubscribeFromScreen', 'startViewingSharedScreen', 'endViewingSharedScreen'],
-  textChat: ['showTextChat', 'hideTextChat', 'messageSent', 'errorSendingMessage', 'messageReceived'],
-  screenSharing: ['startScreenSharing', 'endScreenSharing', 'screenSharingError'],
-  annotation: ['startAnnotation', 'linkAnnotation', 'resizeCanvas', 'annotationWindowClosed', 'endAnnotation'],
-  archiving: ['startArchive', 'stopArchive', 'archiveReady', 'archiveError']
+  session: ['archiveStarted', 'archiveStopped', 'connectionCreated', 'connectionDestroyed', 'sessionConnected', 'sessionDisconnected', 'sessionReconnected', 'sessionReconnecting', 'signal', 'streamCreated', 'streamDestroyed', 'streamPropertyChanged']
 };
 
 module.exports = events;
